3e103c9f820c1007b2f4fb1c812066ad83ea9b5d
BillStephens2022 <two4onebill@yahoo.com>
2023-01-10 09:04:19 -0500

    added functionality to delete products, categories, and tags by ID, added error handling to PUT requests

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,7 +37,8 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  // update a category by its `id` valuea
+  try {
   const categoryData = await Category.update(
     {
       category_name: req.body.category_name,
@@ -49,10 +50,23 @@ router.put('/:id', async (req, res) => {
     },
   );
   res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -50,6 +50,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
+  try {
   const tagData = await Tag.update(
     {
       tag_name: req.body.tag_name,
@@ -61,10 +62,23 @@ router.put('/:id', async (req, res) => {
     },
   );
   res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(tagData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
